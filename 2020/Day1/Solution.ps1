8013b8aca2ec2546f981adcbecb56d13e4ab1c77
CooperWBC <39117442+CooperWBC@users.noreply.github.com>
2020-11-30 22:11:32 -0800

    2020 Day 1 Solutions

---
+++ 2020/Day1/Solution.ps1
@@ -0,0 +1,49 @@
+﻿cls
+
+#
+# Puzzle 1
+#
+
+[int[]]$Numbers = Get-Content -Path input.txt
+
+:outer foreach($Number in $Numbers)
+    {
+        $Number_1 = $Number
+        foreach($Number in $Numbers)
+            {
+                $Number_2 = $Number
+                if(($Number_1 + $Number_2) -eq 2020)
+                    {
+                        Write-Host "$Number_1 + $Number_2 = $($Number_1 + $Number_2)"
+                        Write-Host "$Number_1 * $Number_2 = $($Number_1 * $Number_2)"
+                        break outer
+                    }
+            }
+    }
+
+#
+# Puzzle 2
+#
+
+Get-Variable Number,Number_* | Remove-Variable
+
+:outer foreach($Number in $Numbers)
+    {
+        $Number_1 = $Number
+        foreach($Number in $Numbers)
+            {
+                $Number_2 = $Number
+
+
+                foreach($Number in $Numbers)
+                    {
+                        $Number_3 = $Number
+                        if(($Number_1 + $Number_2 + $Number_3) -eq 2020)
+                            {
+                                Write-Host "$Number_1 + $Number_2 + $Number_3 = $($Number_1 + $Number_2 + $Number_3)"
+                                Write-Host "$Number_1 * $Number_2 * $Number_3 = $($Number_1 * $Number_2 * $Number_3)"
+                                break outer
+                            }
+                    }
+            }
+    }
